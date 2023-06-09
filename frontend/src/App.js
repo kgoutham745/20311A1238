86437e857726b7b47673adfdf184e78314e5db18
kgoutham745 <83576777+kgoutham745@users.noreply.github.com>
2023-06-09 21:00:11 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,16 @@
+import NavBar from './components/NavBar';
+import Schedule from './components/Schedule';
 
+
+function App() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div className="m-5">
+        <Schedule></Schedule>
+      </div>
+    </div>
+  );
+}
+
+export default App;
